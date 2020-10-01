0e3ae526188f1901ec13c12a90d1fda3e675f2fd
Ankur <ankuratsh15@gmail.com>
2020-10-01 00:09:20 -0500

    sol 1

---
+++ 1.py
@@ -0,0 +1,25 @@
+import argparse
+
+#region commandLineArgs
+parser = argparse.ArgumentParser()
+parser.add_argument("N", help="max number", type=int, default=1000)
+args = parser.parse_args()      #Parse arguments
+
+#endregion
+
+class Solution:
+    def __init__(self,N):
+        self.sum = 0
+        self.N = N
+
+    def solve(self):
+        for i in range(self.N):
+            if i%3 == 0 or i%5 == 0:
+                self.sum += i
+        return self.sum
+
+
+if __name__ == "__main__":
+    print("Project Euler Problem 1")
+    res = Solution(args.N)
+    print(res.solve())
