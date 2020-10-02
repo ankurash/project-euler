e122fd41a5c44ae911286862ce95ce30d7259038
ankurs <ankurs@xworkshd.com>
2020-10-02 11:44:55 -0500

    added sol2 on 3.py; minor changes; updated gitignore

--- libraries/mathprimality.py
+++ libraries/mathprimality.py
@@ -2,18 +2,18 @@ import math
 
 def largest_prime_factor(N):
     while N%2==0:
-        N=N/2
+        N=N//2
     for i in range(3,int(math.sqrt(N)),2):
         while N%i==0 and N!=i:
-            N=N/i
-    return int(N)
+            N=N//i
+    return N
 
 def factorize(N):
     factor_pairs = []
     i = 1
-    L = N/2
+    L = N//2
     while i<L:
-        L = int(N/i)
+        L = N//i
         if N%i==0:
             factor_pairs.append((i,L))
         i += 1
