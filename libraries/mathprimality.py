8c66552bceccf0679351fb68c3ef6931d764b621
Ankur <ankuratsh15@gmail.com>
2020-10-04 18:58:34 -0500

    sol5

--- libraries/mathprimality.py
+++ libraries/mathprimality.py
@@ -8,13 +8,33 @@ def largest_prime_factor(N):
             N=N//i
     return N
 
-def factorize(N):
-    factor_pairs = []
-    i = 1
-    L = N//2
-    while i<L:
-        L = N//i
-        if N%i==0:
-            factor_pairs.append((i,L))
-        i += 1
-    return factor_pairs
+def factorize(N, pairs=True):
+    if pairs:
+        factor_pairs = []
+        i = 1
+        L = N
+        while i<L:
+            L = N//i
+            if N%i==0:
+                factor_pairs.append((i,L))
+            i += 1
+        return factor_pairs
+    else:
+        factors = [1]
+        i = 1
+        L = N
+        while i<L:
+            L = N//i
+            if N%i==0:
+                if i not in factors:
+                    factors.append(i)
+                if L not in factors:
+                    factors.append(L)
+            i += 1
+        return factors
+
+def hcf(a,b):
+    return max(set(factorize(a,pairs=False)).intersection(factorize(b,pairs=False)))
+
+
+    
